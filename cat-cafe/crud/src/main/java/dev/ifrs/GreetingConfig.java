1ee41d952d45a17644aae615937b6c10d4065b2e
taisegoulart <taise.m.goulart@gmail.com>
2023-06-08 23:04:32 -0300

    Reestruturação dos diretórios

---
+++ cat-cafe/crud/src/main/java/dev/ifrs/GreetingConfig.java
@@ -0,0 +1,12 @@
+package dev.ifrs;
+
+import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithName;
+
+@ConfigMapping(prefix = "greeting")
+public interface GreetingConfig {
+
+    @WithName("message")
+    String message();
+
+}
