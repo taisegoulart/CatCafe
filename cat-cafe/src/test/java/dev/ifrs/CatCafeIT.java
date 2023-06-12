15baa3bfa0666496b38e61f389830bf38ac8d569
taisegoulart <taise.m.goulart@gmail.com>
2023-06-12 00:42:40 -0300

    Implementando quase todo o CRUD da aplicação

--- cat-cafe/src/test/java/dev/ifrs/CatCafeIT.java
+++
@@ -1,8 +0,0 @@
-package dev.ifrs;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class CatCafeIT extends CatCafeTest {
-    // Execute the same tests but in packaged mode.
-}
